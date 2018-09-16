4aded42f713ac0c6fce7ee1c19539cb3f09534ee
寒枫 <34372490+Han-Fen@users.noreply.github.com>
2018-09-16 09:27:54 +0800

    Update DB.php

--- FnKu/Conf/DB.php
+++ FnKu/Conf/DB.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * (C)2015-2099 FnTie All rights reserved.
- * FnMB version:1.0.3
+ * FnMB version:1.0.4
  * Author: HanFen
  **/
 $Host = '127.0.0.1';
-$DBUser = 'FnMB';
-$DBPwd = '1091044631';
-$DBName = 'FnB';
+$DBUser = '';
+$DBPwd = '';
+$DBName = '';
